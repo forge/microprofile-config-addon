a197b1e4a293a8af02724b73c28d9865afe0dbf5
George Gastaldi <gegastaldi@gmail.com>
2020-02-27 10:01:24 -0300

    Support configsource.properties.url and match Configuration addon behavior

--- src/main/java/org/jboss/forge/addon/config/ForgeConfigSourceProvider.java
+++ src/main/java/org/jboss/forge/addon/config/ForgeConfigSourceProvider.java
@@ -1,10 +1,14 @@
 package org.jboss.forge.addon.config;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,17 +21,42 @@ public class ForgeConfigSourceProvider implements ConfigSourceProvider {
 
     private static final Logger log = Logger.getLogger(ForgeConfigSourceProvider.class.getName());
 
+    private static final String USER_CONFIG_PATH = "org.jboss.forge.addon.configuration.USER_CONFIG_PATH";
+    private static final String CONFIGSOURCE_PROPERTIES_URL_KEY = "configsource.properties.url";
+
     @Override
     public Iterable<ConfigSource> getConfigSources(ClassLoader forClassLoader) {
-        List<ConfigSource> sources = new ArrayList<>(1);
-        Path configProperties = OperatingSystemUtils.getUserForgeDir().toPath().resolve("config.properties");
+        List<ConfigSource> sources = new ArrayList<>(2);
+        Path configProperties = getConfigPropertiesPath();
         if (Files.exists(configProperties)) {
-            try {
-                sources.add(new PropertiesConfigSource(configProperties.toUri().toURL()));
+            Properties props = new Properties();
+            try (InputStream stream = Files.newInputStream(configProperties)) {
+                props.load(stream);
             } catch (IOException e) {
-                log.log(Level.WARNING, "Could not read "+configProperties);
+                log.log(Level.WARNING, "Could not read " + configProperties, e);
+            }
+            sources.add(new PropertiesConfigSource(props, configProperties.toString()));
+            String configSourcePropertiesUrl = props.getProperty(CONFIGSOURCE_PROPERTIES_URL_KEY);
+            if (configSourcePropertiesUrl != null) {
+                try {
+                    sources.add(new PropertiesConfigSource(new URL(configSourcePropertiesUrl)));
+                } catch (IOException e) {
+                    log.log(Level.WARNING, "Could not read " + configSourcePropertiesUrl, e);
+                }
             }
         }
         return sources;
+    }
+
+
+    private Path getConfigPropertiesPath() {
+        String property = System.getProperty(USER_CONFIG_PATH);
+        final File userConfigurationFile;
+        if (property == null || property.isEmpty()) {
+            userConfigurationFile = new File(OperatingSystemUtils.getUserForgeDir(), "config.properties");
+        } else {
+            userConfigurationFile = new File(property);
+        }
+        return userConfigurationFile.toPath();
     }
 }
